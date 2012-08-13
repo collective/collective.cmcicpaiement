44940a3948cbd42d9b3308235481d51da4d0921e
JeanMichel FRANCOIS <toutpt@gmail.com>
2012-08-13 17:41:45 +0200

    add settings

--- collective/cmcicpaiement/i18n.py
+++ collective/cmcicpaiement/i18n.py
@@ -4,6 +4,6 @@ message_factory = MessageFactory("collective.cmcicpaiement")
 _ = message_factory
 
 retour_MAC_desc_def = u"Sceau resultant de la certification des donnees"
-retour_MAC_desc = _(u"retour_MAC_description", retour_MAC_description_def)
+retour_MAC_desc = _(u"retour_MAC_description", retour_MAC_desc_def)
 
 retour_numauto_desc = _(u"Only if the paiement is authorized")
--- collective/cmcicpaiement/settings.py
+++ collective/cmcicpaiement/settings.py
@@ -4,71 +4,68 @@ from zope import interface
 
 #others
 from collective.cmcicpaiement import i18n
+from zope.schema.vocabulary import SimpleVocabulary, SimpleTerm
 
 _ = i18n.message_factory
-contact_source_vocab = ['member', 'creator']
-
-url_paiement_vocabulary = SimpleVocabulary([
-    SimpleTerm("https://paiement.creditmutuel.fr/test/paiement.cgi",
-               "https://paiement.creditmutuel.fr/test/paiement.cgi",
-               u"creditmutuel TEST"),
-    SimpleTerm("https://paiement.creditmutuel.fr/paiement.cgi",
-               "https://paiement.creditmutuel.fr/paiement.cgi",
-               u"creditmutuel PRODUCTION"),
-    SimpleTerm("https://ssl.paiement.cic-banques.fr/test/paiement.cgi",
-               "https://ssl.paiement.cic-banques.fr/test/paiement.cgi",
-               u"cic-banques TEST"),
-    SimpleTerm("https://ssl.paiement.cic-banques.fr/paiement.cgi",
-               "https://ssl.paiement.cic-banques.fr/paiement.cgi",
-               u"cic-banques PRODUCTION"),
-    SimpleTerm("https://ssl.paiement.banque-obc.fr/test/paiement.cgi",
-               "https://ssl.paiement.banque-obc.fr/test/paiement.cgi",
-               u"banque-obc TEST"),
-    SimpleTerm("https://ssl.paiement.banque-obc.fr/paiement.cgi",
-               "https://ssl.paiement.banque-obc.fr/paiement.cgi",
-               u"banque-obc PRODUCTION"),
+contact_source_vocab = SimpleVocabulary([
+    SimpleTerm('member', 'member', u'Authenticated Member'),
+    SimpleTerm('creator', 'creator', u'Creator of the context')
 ])
 
-url_capture_vocabulary = SimpleVocabulary([
-    SimpleTerm("https://paiement.creditmutuel.fr/test/capture_paiement.cgi",
-               "https://paiement.creditmutuel.fr/test/capture_paiement.cgi",
-               u"creditmutuel TEST"),
-    SimpleTerm("https://paiement.creditmutuel.fr/capture_paiement.cgi",
-               "https://paiement.creditmutuel.fr/capture_paiement.cgi",
-               u"creditmutuel PRODUCTION"),
-    SimpleTerm("https://ssl.paiement.cic-banques.fr/test/paiement.cgi",
-               "https://ssl.paiement.cic-banques.fr/test/paiement.cgi",
-               u"cic-banques TEST"),
-    SimpleTerm("https://ssl.paiement.cic-banques.fr/capture_paiement.cgi",
-               "https://ssl.paiement.cic-banques.fr/capture_paiement.cgi",
-               u"cic-banques PRODUCTION"),
-    SimpleTerm("https://ssl.paiement.banque-obc.fr/test/capture_paiement.cgi",
-               "https://ssl.paiement.banque-obc.fr/test/capture_paiement.cgi",
-               u"banque-obc TEST"),
-    SimpleTerm("https://ssl.paiement.banque-obc.fr/capture_paiement.cgi",
-               "https://ssl.paiement.banque-obc.fr/capture_paiement.cgi",
-               u"banque-obc PRODUCTION"),
-])
 
-url_recredit_vocabulary = SimpleVocabulary([
-    SimpleTerm("https://paiement.creditmutuel.fr/test/recredit_paiement.cgi",
-               "https://paiement.creditmutuel.fr/test/recredit_paiement.cgi",
-               u"creditmutuel TEST"),
-    SimpleTerm("https://paiement.creditmutuel.fr/recredit_paiement.cgi",
-               "https://paiement.creditmutuel.fr/recredit_paiement.cgi",
-               u"creditmutuel PRODUCTION"),
-    SimpleTerm("https://ssl.paiement.cic-banques.fr/test/recredit_paiement.cgi",
-               "https://ssl.paiement.cic-banques.fr/test/recredit_paiement.cgi",
-               u"cic-banques TEST"),
-    SimpleTerm("https://ssl.paiement.cic-banques.fr/recredit_paiement.cgi",
-               "https://ssl.paiement.cic-banques.fr/recredit_paiement.cgi",
-               u"cic-banques PRODUCTION"),
-    SimpleTerm("https://ssl.paiement.banque-obc.fr/test/recredit_paiement.cgi",
-               "https://ssl.paiement.banque-obc.fr/test/recredit_paiement.cgi",
-               u"banque-obc TEST"),
-    SimpleTerm("https://ssl.paiement.banque-obc.fr/recredit_paiement.cgi",
-               "https://ssl.paiement.banque-obc.fr/recredit_paiement.cgi",
-               u"banque-obc PRODUCTION"),
+URLS = {
+    "https://paiement.creditmutuel.fr/test/": {
+        "recredit": "https://paiement.creditmutuel.fr/test/recredit_paiement.cgi",
+        "capture" : "https://paiement.creditmutuel.fr/test/capture_paiement.cgi",
+        "paiement" : "https://paiement.creditmutuel.fr/test/paiement.cgi"
+    },
+    "https://paiement.creditmutuel.fr/": {
+        "recredit": "https://paiement.creditmutuel.fr/recredit_paiement.cgi",
+        "capture" : "https://paiement.creditmutuel.fr/capture_paiement.cgi",
+        "paiement" : "https://paiement.creditmutuel.fr/paiement.cgi"
+    },
+    "https://ssl.paiement.cic-banques.fr/test/": {
+        "recredit": "https://ssl.paiement.cic-banques.fr/test/recredit_paiement.cgi",
+        "capture" : "https://ssl.paiement.cic-banques.fr/test/capture_paiement.cgi",
+        "paiement" : "https://ssl.paiement.cic-banques.fr/test/paiement.cgi"
+    },
+    "https://ssl.paiement.cic-banques.fr/": {
+        "recredit": "https://ssl.paiement.cic-banques.fr/recredit_paiement.cgi",
+        "capture" : "https://ssl.paiement.cic-banques.fr/capture_paiement.cgi",
+        "paiement" : "https://ssl.paiement.cic-banques.fr/paiement.cgi"
+    },
+    "https://ssl.paiement.banque-obc.fr/test/": {
+        "recredit": "https://ssl.paiement.banque-obc.fr/test/recredit_paiement.cgi",
+        "capture" : "https://ssl.paiement.banque-obc.fr/test/capture_paiement.cgi",
+        "paiement" : "https://ssl.paiement.banque-obc.fr/test/paiement.cgi"
+    },
+    "https://ssl.paiement.banque-obc.fr/": {
+        "recredit": "https://ssl.paiement.banque-obc.fr/recredit_paiement.cgi",
+        "capture" : "https://ssl.paiement.banque-obc.fr/capture_paiement.cgi",
+        "paiement" : "https://ssl.paiement.banque-obc.fr/paiement.cgi"
+    }
+}
+
+
+bank_vocabulary = SimpleVocabulary([
+    SimpleTerm("https://paiement.creditmutuel.fr/test/",
+               "https://paiement.creditmutuel.fr/test/",
+               u"Credit Mutuel TEST"),
+    SimpleTerm("https://paiement.creditmutuel.fr/",
+               "https://paiement.creditmutuel.fr/",
+               u"Credit Mutuel PRODUCTION"),
+    SimpleTerm("https://ssl.paiement.cic-banques.fr/test/",
+               "https://ssl.paiement.cic-banques.fr/test/",
+               u"CIC banques TEST"),
+    SimpleTerm("https://ssl.paiement.cic-banques.fr/",
+               "https://ssl.paiement.cic-banques.fr/",
+               u"CIC banques PRODUCTION"),
+    SimpleTerm("https://ssl.paiement.banque-obc.fr/test/",
+               "https://ssl.paiement.banque-obc.fr/test/",
+               u"Banque-obc TEST"),
+    SimpleTerm("https://ssl.paiement.banque-obc.fr/",
+               "https://ssl.paiement.banque-obc.fr/",
+               u"Banque-obc PRODUCTION"),
 ])
 
 
@@ -79,19 +76,12 @@ class Settings(interface.Interface):
 
     TPE = schema.ASCIILine(title=_(u"TPE"))
 
-    contact_source = schema.ASCIILine(title=_(u"Contact source"),
-                                      vocabulary=contact_source_vocab)
-
     societe = schema.ASCIILine(title=_(u"Societe number"))
 
-    url_paiement = schema.URI(title=_(u"URL Paiement"),
-                              vocabulary=url_paiement_vocabulary,
-              default="https://paiement.creditmutuel.fr/test/paiement.cgi")
+    bank = schema.Choice(title=_(u"Bank"),
+                      vocabulary=bank_vocabulary,
+                      default="https://paiement.creditmutuel.fr/test/")
 
-    url_capture = schema.URI(title=_(u"URL Capture"),
-                             vocabulary=url_capture_vocabulary,
-            default=DEFAULT_URL_CM['capture'])
+    contact_source = schema.Choice(title=_(u"Contact source"),
+                                      vocabulary=contact_source_vocab)
 
-    url_recredit = schema.URI(title=_(u"URL Recredit"),
-                              vocabulary=url_recredit_vocabulary,
-            default=DEFAULT_URL_CM['recredit'])
